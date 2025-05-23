ce4d346dad6b910d9264d8df25c64e32b00ad897
Sergej A <sergbest@mail.ru>
2025-05-23 09:57:45 +0300

    create project

---
+++ source/router.js
@@ -0,0 +1,53 @@
+import { Router, urlencoded, static as staticMiddleware } from "express";
+import methodOverride from 'method-override';
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import _FileStore from 'session-file-store';
+import { flash } from "express-flash-message";
+
+import { mainPage, detailPage, addPage, add, setDone, remove, setOrder, addendumWrapper } from './controllers/todos.js';
+import { requestToContext, handleErrors, extendFlashAPI, getErrors } from "./middleware.js";
+import { todoV } from "./validators.js";
+import { mainErrorHandler, error500Handler} from './error-handlers.js';
+
+const FileStore = _FileStore(session);
+const router = Router();
+
+router.use(staticMiddleware('public'));
+router.use(urlencoded({extended: true}));
+router.use(methodOverride('_method'));
+
+router.use(cookieParser());
+router.use(requestToContext);
+
+router.use(session({
+  store: new FileStore({
+    path: './storage/sessions',
+    reapAsync: true,
+    reapSyncFallback: true,
+    fallbackSessionFn: () => {
+      return {};
+    },
+    logFn: () => {}
+  }),
+  secret: 'abcdefgh',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  }
+}));
+router.use(flash({sessionKeyName: 'flash-message'}));
+router.use(extendFlashAPI);
+
+router.get('/add', getErrors, addPage);
+router.post('/add', addendumWrapper, todoV, handleErrors, add);
+router.get('/:id', detailPage);
+router.put('/:id', setDone);
+router.delete('/:id', remove);
+router.post('/setorder', setOrder);
+router.get('/', mainPage);
+
+router.use(mainErrorHandler, error500Handler);
+
+export default router;
