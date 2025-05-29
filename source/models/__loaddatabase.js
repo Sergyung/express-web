7a44d820e4b8f3c49d2e591d73fa1dea234481e1
Serg A <sergbest@mail.ru>
2025-05-29 13:24:28 +0300

    connection datebase

--- source/models/__loaddatabase.js
+++ source/models/__loaddatabase.js
@@ -1,23 +1,47 @@
-import { join } from 'node:path';
-import { readFileSync } from 'node:fs';
-import { writeFile } from 'node:fs/promises';
+import { connect, Schema, model } from "mongoose";
 
-import { currentDir } from '../utility.js';
+const dbURI = process.env.DBURI || 'mongodb://127.0.0.1:27017/';
+const dbName = process.env.DBNAME || 'todos';
 
-const dataFileName = join(currentDir, 'data', 'todos.json');
+const scTodo = new Schema({
+    title: String,
+    desc: String,
+    addendum: String,
+    user: {
+        type: Schema.Types.ObjectId,
+        index: true
+    },
+    done: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        index: true,
+        default: () => new Date()
+    }
+}, {
+    versionKey: false
+});
+scTodo.index({done: 1, createdAt: 1});
 
-const dataFile = readFileSync(dataFileName, 'utf8');
-const database = JSON.parse(dataFile);
-export { database };
+const scUser = new Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: Buffer,
+    salt: Buffer
+}, {
+    versionKey: false
+});
 
-export function saveDatabase(){
-    const s = JSON.stringify(database);
-    writeFile(dataFileName, s, 'utf8');
-};
+let User, Todo;
 
-export function getObjectId(){
-    const timestamp = (new Date().getTime()/1000 | 0).toString(16);
-    return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, () => {
-        return (Math.random() * 16 | 0).toString(16);
-    }).toLowerCase();
-};
+export async function connectToDB() {
+    await connect(dbURI, { dbName: dbName});
+    Todo = model('Todo', scTodo);
+    User = model('User', scUser);
+}
+
+export { Todo, User};
